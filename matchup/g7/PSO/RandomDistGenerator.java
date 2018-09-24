f25ee6c343a4ce149ce80fd8cf95e56a803cbaf0
Warren <wmowkm@gmail.com>
2018-09-24 18:01:32 -0400

    bugfix

--- matchup/g7/PSO/RandomDistGenerator.java
+++ matchup/g7/PSO/RandomDistGenerator.java
@@ -16,49 +16,71 @@ public class RandomDistGenerator {
     // TESTING
     //public static void main(String[] args) {
     public ArrayList<Integer> randomDist() {
+        ArrayList<Integer> skills;
+        ArrayList<Integer> elevens;
+        HashMap<Integer, Integer> players_skills;
+
+        int seed = 0;
+        Random rand;
+
         Integer max;
+
         Integer player;
         Integer skill;
 
-        rand = new Random(seed);
+        rand = new Random();
 
         players_skills = new HashMap<Integer, Integer>();
-        for (int i = 0; i < 15; i++) {
-            players_skills.put(i,1);
+        for (int k = 0; k < 15; k++) {
+            players_skills.put(k,1);
         }
 
         skills = new ArrayList<Integer>();
         elevens = new ArrayList<Integer>();
-        for (int i = 0; i < 74; i++) {
+        for (Integer i = 0; i < 75; i++) {
             max = 15 - skills.size();
-            player = rand.nextInt(15);
+            player = rand.nextInt(max);
 
-            for (int p : elevens) {
+            for (Integer p : elevens) {
                 if (player >= p) {
                     player++;
                 }
             }
-
             skill = players_skills.get(player);
+            for (Integer name : players_skills.keySet()) {
+                String key = name.toString();
+                String value = players_skills.get(name).toString();
+            }
             players_skills.put(player, ++skill);
 
-            if (skill == 10) {
+            if (skill == 11) {
                 players_skills.remove(player);
                 skills.add(11);
                 elevens.add(player);
                 Collections.sort(elevens);
             }
 
+            /*TESTING
+            int n = 0;
+            for (int l : players_skills.values()) {
+                n += l;
+            }
+            for (int m : elevens) {
+                n += m;
+            }
+            System.out.printf("%d\n", n);
+            */
+
         }
 
-        for (int i : players_skills.values()) {
-            skills.add(i);
+        for (int j : players_skills.values()) {
+            skills.add(j);
         }
 
-        /* TESTING
+        /*TESTING
         int n = 0;
         for (int i : skills) {
-            System.out.printf("%d\n", i);
+            //System.out.printf("%d\n", i);
             n += i;
         }
         System.out.printf("%d\n", n);
