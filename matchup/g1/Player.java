bb68bab36c4705523b923ebec31c9dab3548a27e
debug-thebug <sahilshah13496@gmail.com>
2018-09-25 03:20:39 -0400

    Refactored code. Distribution is created from history. Low and Mixed variance Teams can be built

--- matchup/g1/Player.java
+++ matchup/g1/Player.java
@@ -30,122 +30,128 @@ public class Player implements matchup.sim.Player {
 
     public List<Integer> getSkills() {
 	    skills = new ArrayList<Integer>();
-	    //Uniform
-	    //Collections.addAll(skills, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 2, 2, 10, 10);
-
-            //Risky
-	    //Collections.addAll(skills, 11, 11, 1, 1, 6);
-	    //Collections.addAll(skills, 11, 11, 1, 1, 6);
-	    //Collections.addAll(skills, 11, 11, 1, 1, 6);
-
-	    //Safe
-	    //Collections.addAll(skills, 8, 8, 4, 4, 6);
-	    //Collections.addAll(skills, 8, 8, 4, 4, 6);
-	    //Collections.addAll(skills, 8, 8, 4, 4, 6);
 	    
-	    //Middle
-	    Collections.addAll(skills, 10, 2, 9, 3, 6);
-	    Collections.addAll(skills, 10, 2, 9, 3, 6);
-	    Collections.addAll(skills, 10, 2, 9, 3, 6);
+	    Collections.addAll(skills, 1, 1, 9, 9, 8);
+	    Collections.addAll(skills, 1, 1, 9, 9, 8);
+	    Collections.addAll(skills, 1, 7, 9, 9, 8);
 	    
 		
 		List<Game> games = History.getHistory();
-		//System.out.println("Number of games played: ");
-		//System.out.println(games.size());
 		
+		if(games.size() > 1){
+			
+			List<Integer> opponentPastSkills = buildFromHistory();	//approximates opponent distribution from history
+			List<Integer> adaptedPlayersSkills = buildOptimalPlayers(opponentPastSkills);	//generates our lineup of 15 players
+			
+    		skills = getLowVarianceLineup(adaptedPlayersSkills);	//returns low variance skills
+    		//skills = getMixedVarianceLineup(adaptedPlayersSkills); //returns mixed variance skills
+		}
+
+	    return skills;
+    }
+    
+    //generate distribution of 15 after learning from history
+    public List<Integer> buildFromHistory()
+    {
+		List<Game> games = History.getHistory();
+    	Game game;
+		PlayerData opponent;
+		PlayerData friendly;
 		
+		List<Integer> opponentAvgPastSkills = new ArrayList<Integer>();
+		Collections.addAll(opponentAvgPastSkills, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+		
+		for(int i=0; i<games.size(); i+=2)
+		{
+			game = games.get(i);
+		
+			if (game.playerA.name == "g1") {
+					friendly = game.playerA;
+					opponent = game.playerB;
+			} else {
+				friendly = game.playerB;
+				opponent = game.playerA;
+			}
+			
+			Collections.sort(opponent.skills);
+			
+			//sum of all previous skills
+			for (int j=0; j<15; j++){
+				opponentAvgPastSkills.set(j, opponentAvgPastSkills.get(j) + opponent.skills.get(j));
+			}
+		}
+		
+		//divide by total games to get adapted skills
+		for (int j=0; j<15; j++){
+			opponentAvgPastSkills.set(j, (opponentAvgPastSkills.get(j)/(games.size()/2)));
+		}
+			
+		return opponentAvgPastSkills;
+    }
+    
+    //generate our distribution to beat the predicted opponent distribution
+    public List<Integer> buildOptimalPlayers(List<Integer> opponentAvgPastSkills)
+    {
 		List<Integer> friendlyNewSkills = new ArrayList<Integer>();
 		Collections.addAll(friendlyNewSkills, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 		
-		if(games.size() > 1){
-			Game game;
-			PlayerData opponent;
-			PlayerData friendly;
+    	for (int j=0; j<15; j++){
 			
-			List<Integer> opponentAvgPastSkills = new ArrayList<Integer>();
-			Collections.addAll(opponentAvgPastSkills, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-			//System.out.println("Opponent Skills in previous games: ");
-			for(int i=0; i<games.size(); i+=2)
-			{
-				game = games.get(i);
-			
-				if (game.playerA.name == "g1") {
-						friendly = game.playerA;
-						opponent = game.playerB;
-						//count_playerA_friendly += 1;
-				} else {
-					friendly = game.playerB;
-					opponent = game.playerA;
-				}
-				
-				//System.out.println(opponent.distribution);
-				//System.out.println(opponent.skills);
-				Collections.sort(opponent.skills);
-				
-				for (int j=0; j<15; j++){
-					opponentAvgPastSkills.set(j, opponentAvgPastSkills.get(j) + opponent.skills.get(j));
-				}
-				
-				//System.out.println(opponent.skills);
-				
-			}
-			
-			for (int j=0; j<15; j++){
-				opponentAvgPastSkills.set(j, (opponentAvgPastSkills.get(j)/(games.size()/2)));
-			}
-			
-			//System.out.println("Games Played: ");
-			//int gamesPlayed = (games.size()/2);
-			//System.out.println(gamesPlayed);
-			//System.out.println("Opponent Average Past Skills: ");
-			//System.out.println(opponentAvgPastSkills);
-			
-			for (int j=0; j<15; j++){
-			
-				if (opponentAvgPastSkills.get(j) <= 6)
-					friendlyNewSkills.set(j, opponentAvgPastSkills.get(j) + 3);
-				else
-					friendlyNewSkills.set(j, opponentAvgPastSkills.get(j) - 2);
-			}
-			
-			int sumOfSkills = 0;
-			for(int j=0; j<15; j++)
-				sumOfSkills += friendlyNewSkills.get(j);
-			System.out.println(sumOfSkills);
-			int diff = 90 - sumOfSkills;
-			
-			//keep adding randomly till total 90 when sum of skills < 90
-			while(diff > 0)
-			{
-				int x = rand.nextInt(15);
-				if (friendlyNewSkills.get(x) < 11)
-				{
-					friendlyNewSkills.set(x, friendlyNewSkills.get(x) + 1);
-					diff--;
-				}
-			}
-			
-			
-			//keep subtracting randomly till total 90 when sum of skills > 90
-			while(diff < 0)
-			{
-				int x = rand.nextInt(15);
-				if (friendlyNewSkills.get(x) > 1)
-				{
-					friendlyNewSkills.set(x, friendlyNewSkills.get(x) - 1);
-					diff++;
-				}
-			}
-			//System.out.println(opponentAvgPastSkills);
-			//System.out.println(friendlyNewSkills);
-			
-			skills = friendlyNewSkills;
-			return skills;
+			if (opponentAvgPastSkills.get(j) <= 6)
+				friendlyNewSkills.set(j, opponentAvgPastSkills.get(j) + 3);
+			else
+				friendlyNewSkills.set(j, opponentAvgPastSkills.get(j) - 2);
 		}
-
-	    //Old
-	    //Collections.addAll(skills, 11, 11, 1, 1, 6, 8, 8, 4, 4, 6, 10, 2, 9, 3, 6);
-	    return skills;
+	
+		int sumOfSkills = 0;
+		for(int j=0; j<15; j++)
+			sumOfSkills += friendlyNewSkills.get(j);
+			
+		int diff = 90 - sumOfSkills;
+	
+		//keep adding randomly till total 90 when sum of skills < 90
+		while(diff > 0)
+		{
+			int x = rand.nextInt(15);
+			if (friendlyNewSkills.get(x) < 11)
+			{
+				friendlyNewSkills.set(x, friendlyNewSkills.get(x) + 1);
+				diff--;
+			}
+		}
+	
+		//keep subtracting randomly till total 90 when sum of skills > 90
+		while(diff < 0)
+		{
+			int x = rand.nextInt(15);
+			if (friendlyNewSkills.get(x) > 1)
+			{
+				friendlyNewSkills.set(x, friendlyNewSkills.get(x) - 1);
+				diff++;
+			}
+		}
+		
+		Collections.sort(friendlyNewSkills);
+		
+		return friendlyNewSkills;
+    }
+    
+    //generate lineup with low variance
+    public List<Integer> getLowVarianceLineup(List<Integer> fskills)
+    {
+    	Collections.sort(fskills);
+    	
+    	return fskills;
+    }
+    
+    //generate lineup with mixed variance
+    public List<Integer> getMixedVarianceLineup(List<Integer> fskills)
+    {
+    	Collections.sort(fskills);
+    	Collections.swap(fskills, 3, 14);
+    	Collections.swap(fskills, 4, 13);
+    	
+    	return fskills;
     }
 
     public List<List<Integer>> getDistribution(List<Integer> opponentSkills, boolean isHome) {
@@ -155,7 +161,6 @@ public class Player implements matchup.sim.Player {
 
     	distribution = new ArrayList<List<Integer>>();
 
-        //Collections.shuffle(index, rand);
 		int n = 0;
     	for (int i=0; i<3; ++i) {
     		List<Integer> row = new ArrayList<Integer>();
@@ -210,36 +215,21 @@ public class Player implements matchup.sim.Player {
             availableRows.remove(n);
            
 	    return round;
-	}
-	//System.out.println("inside playRound");
-    	//System.out.println("oppTeam:");
-	//System.out.println(opponentRound);
-	//System.out.println("bestTeam:");
-	//System.out.println(bestTeam);
-	int n = chooseOptimalLineup(opponentRound);
+		}
+	
+		int n = chooseOptimalLineup(opponentRound);
 
-    	List<Integer> round = new ArrayList<Integer>(distribution.get(availableRows.get(n)));
-    	availableRows.remove(n);
+		List<Integer> round = new ArrayList<Integer>(distribution.get(availableRows.get(n)));
+		availableRows.remove(n);
 
-	bestTeam = new ArrayList<Integer>();
-	for (int i : round) {
-	    Collections.addAll(bestTeam, i);
-	}
-	//Collections.addAll(bestTeam, 0, 0, 0, 0, 0);
-	//System.out.println("bestTeam before permute:");
-	//System.out.println(bestTeam);
+		bestTeam = new ArrayList<Integer>();
+		for (int i : round) {
+			Collections.addAll(bestTeam, i);
+		}
 
-	round = permuteHomeTeam(round, opponentRound);
-	//System.out.println("oppTeam:");
-	//System.out.println(opponentRound);
-	System.out.println("g1 bestTeam:");
-	System.out.println(bestTeam);
-	System.out.println("g1 round:");
-	System.out.println(round);
-	return bestTeam;
-	//round = bestTeam;
-
-    	//return round;
+		round = permuteHomeTeam(round, opponentRound);
+		return bestTeam;
+	
     }
 
     public void clear() {
@@ -248,121 +238,107 @@ public class Player implements matchup.sim.Player {
 
         // Get history of games.
         List<Game> games = History.getHistory();
-	int friendly_away_score = 0;
-	int opponent_away_score = 0;
-	int friendly_away_wins = 0;
-	int opponent_away_wins = 0;
-	int friendly_home_score = 0;
-	int opponent_home_score = 0;
-	int friendly_home_wins = 0;
-	int opponent_home_wins = 0;
-	int friendly_total_score = 0;
-	int opponent_total_score = 0;
-	int friendly_total_wins = 0;
-	int opponent_total_wins = 0;
-	int numGames = 2000;
-	//int count_playerA_friendly = 0;
-	if (games.size() >= numGames) {
-	    System.out.println(games.size());
-	    Game game;
-	    PlayerData opponent;
-	    PlayerData friendly;
-	    for (int i = 0; i < numGames; i++) {
-		game = games.get(i);
-		if (game.playerA.name == "g1") {
-		    friendly = game.playerA;
-		    opponent = game.playerB;
-		    //count_playerA_friendly += 1;
-		} else {
-		    friendly = game.playerB;
-		    opponent = game.playerA;
-		}
-		if (friendly.score > opponent.score) {
-		    friendly_total_wins += 1;
-		} else if (friendly.score < opponent.score) {
-		    opponent_total_wins += 1;
-		}
-		friendly_total_score += friendly.score;
-		opponent_total_score += opponent.score;
-		if (friendly.isHome) {
-		    if (friendly.score > opponent.score) {
-			friendly_home_wins += 1;
-		    } else if (friendly.score < opponent.score) {
-			opponent_home_wins += 1;
-		    }
-		    friendly_home_score += friendly.score;
-		    opponent_home_score += opponent.score;
-		}
-		if (!friendly.isHome) {
-		    if (friendly.score > opponent.score) {
-			friendly_away_wins += 1;
-		    } else if (friendly.score < opponent.score) {
-			opponent_away_wins += 1;
-		    }
-		    friendly_away_score += friendly.score;
-		    opponent_away_score += opponent.score;
-		}
-	    }
-	    
-	    
-	    System.out.println("end result:");
+		int friendly_away_score = 0;
+		int opponent_away_score = 0;
+		int friendly_away_wins = 0;
+		int opponent_away_wins = 0;
+		int friendly_home_score = 0;
+		int opponent_home_score = 0;
+		int friendly_home_wins = 0;
+		int opponent_home_wins = 0;
+		int friendly_total_score = 0;
+		int opponent_total_score = 0;
+		int friendly_total_wins = 0;
+		int opponent_total_wins = 0;
+		int numGames = 2000;
+		//int count_playerA_friendly = 0;
+		if (games.size() >= numGames) {
+			System.out.println(games.size());
+			Game game;
+			PlayerData opponent;
+			PlayerData friendly;
+			for (int i = 0; i < numGames; i++) {
+			game = games.get(i);
+			if (game.playerA.name == "g1") {
+				friendly = game.playerA;
+				opponent = game.playerB;
+				//count_playerA_friendly += 1;
+			} else {
+				friendly = game.playerB;
+				opponent = game.playerA;
+			}
+			if (friendly.score > opponent.score) {
+				friendly_total_wins += 1;
+			} else if (friendly.score < opponent.score) {
+				opponent_total_wins += 1;
+			}
+			friendly_total_score += friendly.score;
+			opponent_total_score += opponent.score;
+			if (friendly.isHome) {
+				if (friendly.score > opponent.score) {
+				friendly_home_wins += 1;
+				} else if (friendly.score < opponent.score) {
+				opponent_home_wins += 1;
+				}
+				friendly_home_score += friendly.score;
+				opponent_home_score += opponent.score;
+			}
+			if (!friendly.isHome) {
+				if (friendly.score > opponent.score) {
+				friendly_away_wins += 1;
+				} else if (friendly.score < opponent.score) {
+				opponent_away_wins += 1;
+				}
+				friendly_away_score += friendly.score;
+				opponent_away_score += opponent.score;
+			}
+			}
+			
+			
+			System.out.println("end result:");
 
-	    System.out.println("home:");
-	    System.out.println("friendly result:");
-	    System.out.println("score:");
-	    System.out.println(friendly_home_score);
-	    System.out.println("wins:");
-	    System.out.println(friendly_home_wins);
-	    System.out.println("opponent result:");
-	    System.out.println("score:");
-	    System.out.println(opponent_home_score);
-	    System.out.println("wins:");
-	    System.out.println(opponent_home_wins);
-	    System.out.println("away:");
-	    System.out.println("friendly result:");
-	    System.out.println("score:");
-	    System.out.println(friendly_away_score);
-	    System.out.println("wins:");
-	    System.out.println(friendly_away_wins);
-	    System.out.println("opponent result:");
-	    System.out.println("score:");
-	    System.out.println(opponent_away_score);
-	    System.out.println("wins:");
-	    System.out.println(opponent_away_wins);
-	    System.out.println("friendly result:");
-	    //System.out.println("count_playerA_friendly:");
-	    //System.out.println(count_playerA_friendly);
-	    System.out.println("score:");
-	    System.out.println(friendly_total_score);
-	    System.out.println("wins:");
-	    System.out.println(friendly_total_wins);
-	    System.out.println("opponent result:");
-	    System.out.println("score:");
-	    System.out.println(opponent_total_score);
-	    System.out.println("wins:");
-	    System.out.println(opponent_total_wins);
-	}
+			System.out.println("home:");
+			System.out.println("friendly result:");
+			System.out.println("score:");
+			System.out.println(friendly_home_score);
+			System.out.println("wins:");
+			System.out.println(friendly_home_wins);
+			System.out.println("opponent result:");
+			System.out.println("score:");
+			System.out.println(opponent_home_score);
+			System.out.println("wins:");
+			System.out.println(opponent_home_wins);
+			System.out.println("away:");
+			System.out.println("friendly result:");
+			System.out.println("score:");
+			System.out.println(friendly_away_score);
+			System.out.println("wins:");
+			System.out.println(friendly_away_wins);
+			System.out.println("opponent result:");
+			System.out.println("score:");
+			System.out.println(opponent_away_score);
+			System.out.println("wins:");
+			System.out.println(opponent_away_wins);
+			System.out.println("friendly result:");
+			//System.out.println("count_playerA_friendly:");
+			//System.out.println(count_playerA_friendly);
+			System.out.println("score:");
+			System.out.println(friendly_total_score);
+			System.out.println("wins:");
+			System.out.println(friendly_total_wins);
+			System.out.println("opponent result:");
+			System.out.println("score:");
+			System.out.println(opponent_total_score);
+			System.out.println("wins:");
+			System.out.println(opponent_total_wins);
+		}
     }
 
     public List<Integer> permuteHomeTeam(List<Integer> homeTeam, List<Integer> awayTeam){
-       	//System.out.println("homeTeam:");
-	//System.out.println(homeTeam);
-       	//System.out.println("awayTeam:");
-	//System.out.println(awayTeam);
-	//System.out.println("bestTeam:");
-	//System.out.println(bestTeam);
-        if(checkLineupScore(homeTeam, awayTeam) == 5){
+        if(checkLineupScore(homeTeam, awayTeam) == 5)
             return bestTeam;
-        }
-       	//System.out.println("homeTeam:");
-	//System.out.println(homeTeam);
-       	//System.out.println("awayTeam:");
-	//System.out.println(awayTeam);
-	//System.out.println("bestTeam:");
-	//System.out.println(bestTeam);
-	permute(homeTeam, awayTeam);
-
-        //System.out.println(checkLineupScore(homeTeam, awayTeam));
+            
+		permute(homeTeam, awayTeam);
         return bestTeam;
     }
 
@@ -371,11 +347,6 @@ public class Player implements matchup.sim.Player {
     }
 
     public void permuteHelper(List<Integer> arr, int index, List<Integer> away){
-	//get rid of temp?
-        if(index >= arr.size() - 1){
-            int temp = 5;
-        }
-
         for(int i = index; i < arr.size(); i++){
             if(checkLineupScore(arr, away) > checkLineupScore(bestTeam, away)){
                 for(int j=0; j < arr.size();j++){
@@ -389,7 +360,6 @@ public class Player implements matchup.sim.Player {
     }
 
     public static int checkLineupScore(List<Integer> homeTeam, List<Integer> awayTeam){
-	// optimize later
         int score = 0;
         for(int i = 0; i < homeTeam.size(); i++){
             if(homeTeam.get(i) > awayTeam.get(i) + 2){
@@ -397,13 +367,12 @@ public class Player implements matchup.sim.Player {
             } else if(awayTeam.get(i) > homeTeam.get(i) + 2) {
                 score--;
             } else if (homeTeam.get(i) == awayTeam.get(i) - 2) {
-		score++;
-	    } else if (awayTeam.get(i) == homeTeam.get(i) - 2) {
-		score--;
-	    }
+			score++;
+	    	} else if (awayTeam.get(i) == homeTeam.get(i) - 2) {
+			score--;
+	    	}
         }
+        
         return score;
     }
-
-
 }
