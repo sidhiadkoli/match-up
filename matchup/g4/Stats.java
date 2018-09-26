428efc7bf309135375efdc1dffce6115748f3b58
Chinmay41018 <chinmayjoshi75@gmail.com>
2018-09-26 03:08:40 -0400

    Adding Counter Detection

--- matchup/g4/Stats.java
+++ matchup/g4/Stats.java
@@ -13,7 +13,9 @@ class Stats {
 	private List<Integer> predOppSkills;
 	private boolean isPlayerA;
 	private int lossStreak;
-
+	private boolean detectedTwoStep = false;
+	private boolean immediateCounter = false;
+	private boolean twoStepCounter = false;
 	public Stats() {
 		games = History.getHistory();
 		isPlayerA = games.get(0).playerA.name.equals("g4");
@@ -26,6 +28,53 @@ class Stats {
         checkLoss();
 	}
 
+	
+	public boolean isEqualList(List<Integer> list1, List<Integer> list2){
+		List<Integer> one = new ArrayList<Integer>(list1); 
+	    List<Integer> two = new ArrayList<Integer>(list2);   
+	    Collections.sort(one);
+    	Collections.sort(two);      
+    	return one.equals(two);
+	}
+
+	public void checkCounterStrategy(){
+		int lastScoreA = games.get(games.size()-1).playerA.score + games.get(games.size()-2).playerA.score;
+        int lastScoreB = games.get(games.size()-1).playerB.score + games.get(games.size()-2).playerB.score;
+        int lastScore = lastScoreB - lastScoreA;
+        if (isPlayerA) {lastScore = -lastScore;}
+		if (lastScore < 0){
+	        List<Integer> opskillsR0 = new ArrayList<Integer>();
+	        List<Integer> opskillsR1 = new ArrayList<Integer>();
+    		List<Integer> ourSkillsR0 = new ArrayList<Integer>();
+    		List<Integer> ourSkillsR1 = new ArrayList<Integer>();
+    		List<Integer> ourSkillsR2 = new ArrayList<Integer>();
+    		if (isPlayerA){
+				opskillsR0 = games.get(games.size()-1).playerB.skills;
+				ourSkillsR0 = games.get(games.size()-1).playerA.skills;
+				opskillsR1 = games.get(games.size()-3).playerB.skills;
+				ourSkillsR1 = games.get(games.size()-3).playerA.skills;
+				ourSkillsR2 = games.get(games.size()-5).playerA.skills;
+    		}
+    		else {
+				opskillsR0 = games.get(games.size()-1).playerA.skills;
+				ourSkillsR0 = games.get(games.size()-1).playerB.skills;
+				opskillsR1 = games.get(games.size()-3).playerA.skills;
+				ourSkillsR1 = games.get(games.size()-3).playerB.skills;
+				ourSkillsR2 = games.get(games.size()-5).playerB.skills;
+	    	}
+	    	if(isEqualList(ourSkillsR0,ourSkillsR1)&&(isEqualList(opskillsR1,counter(ourSkillsR2)))&&(isEqualList(opskillsR0,counter(ourSkillsR1)))) {
+	    		immediateCounter = true;
+	    		twoStepCounter = false;
+	    		detectedTwoStep = false;
+	    	}
+			else if(isEqualList(ourSkillsR0,ourSkillsR1)&&isEqualList(ourSkillsR0,ourSkillsR2)&&(isEqualList(opskillsR0,counter(ourSkillsR2)))) {
+				twoStepCounter = true;
+				detectedTwoStep = true;
+				immediateCounter = false;
+			}
+		}
+	}
+	
 	private void checkLoss() {
         int lastScoreA = games.get(games.size()-1).playerA.score + games.get(games.size()-2).playerA.score;
         int lastScoreB = games.get(games.size()-1).playerB.score + games.get(games.size()-2).playerB.score;
@@ -44,6 +93,26 @@ class Stats {
     }
 
     public Skills getCounter() {
+    	
+    	//checkCounterStrategy();
+    	if(immediateCounter){
+    		List<Integer> opskillsR0 = new ArrayList<Integer>();
+    		if(!isPlayerA) opskillsR0 = games.get(games.size()-1).playerA.skills;
+    		else opskillsR0 = games.get(games.size()-1).playerB.skills;
+    		return new Skills(counter(opskillsR0));
+    	}
+    	else if(twoStepCounter){
+    		List<Integer> ourSkillsR0 = new ArrayList<Integer>();
+    		if(!isPlayerA) ourSkillsR0 = games.get(games.size()-1).playerB.skills;
+			else ourSkillsR0 = games.get(games.size()-1).playerA.skills;
+    		if (detectedTwoStep==true){
+	    		detectedTwoStep = !detectedTwoStep;
+    			return new Skills(counter(counter(ourSkillsR0)));
+    		}
+    		detectedTwoStep = !detectedTwoStep;
+    		return new Skills(ourSkillsR0);
+    	}
+    	
     	lossStreak = 0; // reset, give new strategy a chance to win
 
         List<List<Integer>> opskills = new ArrayList<List<Integer>>();
